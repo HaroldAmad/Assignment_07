2daa64f93ac02ab21972287f9db5f9ca029a49ab
HaroldAmad <harold.amad@gmail.com>
2021-12-01 16:22:20 +0800

    Vowels and Consonants Value and Index

--- Program01.py
+++ Program01.py
@@ -5,3 +5,10 @@ print("Character Checker")
 def counter ():
     sentenceF = input("Please input your sentence here: ")
     wordCount = len(sentenceF.split(" "))
+
+    vowelsCount = 0
+    consonantsCount = 0
+
+    vowels =  re.split(r"\s", "a e i o u")
+    consonants = re.split(r"\s", "b c d f g h j k l m n p q r s t v w x y z")
+    
