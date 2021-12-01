ed31f4c715b622030a94baaba963161745af084b
HaroldAmad <harold.amad@gmail.com>
2021-12-01 17:19:20 +0800

    Index and Values

--- Program02.py
+++ Program02.py
@@ -6,3 +6,10 @@ print("Password Checker")
 def checker ():
     passwordF = input("Please input your password here: ")
     legendCount = len(passwordF)
+
+    validPoint = 0
+
+    capital = string.ascii_uppercase
+    number = re.split(r"\s", "0 1 2 3 4 5 6 7 8 9")
+    specialCharacter = re.split(r"\s", "! # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` ¿ ¬ « » ° • · ‘ ’ “ ” – — ")
+
