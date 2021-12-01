05ebd9879ec69437cee4b05ddfa9fe2492ade790
HaroldAmad <harold.amad@gmail.com>
2021-12-01 17:21:00 +0800

    More than 15 letters

--- Program02.py
+++ Program02.py
@@ -13,3 +13,8 @@ def checker ():
     number = re.split(r"\s", "0 1 2 3 4 5 6 7 8 9")
     specialCharacter = re.split(r"\s", "! # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` ¿ ¬ « » ° • · ‘ ’ “ ” – — ")
 
+    if legendCount >= 15:
+        validPoint += 1
+    else:
+        validPoint += 0
+
